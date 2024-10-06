edb50db9b545a27335de666ec0fd998ec00a6e7d
Precious Olusola <preciousolusola16@gmail.com>
2024-10-06 11:01:51 +0100

    Updated Meta datafor link

--- src/app/vote/layout.tsx
+++ src/app/vote/layout.tsx
@@ -1,6 +1,13 @@
 import { Fragment } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "FYB Award Spotlight | Reddemed Christian Fellowship FUTA",
+  description: "FYB Hive Feast 2024: Splendor Of His Love",
+};
 
 export default function FixedWidthLayout({
     children,
