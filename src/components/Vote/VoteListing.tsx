b33b20969b400501e4295c68a59bbf9815ab725d
Precious Olusola <preciousolusola16@gmail.com>
2024-10-08 15:33:44 +0100

    Added Pronoun to the names in cliques

--- src/components/Vote/VoteListing.tsx
+++ src/components/Vote/VoteListing.tsx
@@ -109,7 +109,7 @@ function VoteCard(props: VoteCardProps) {
             <div className="voter-card-name">
                 {/* <div className="shimmer"></div> */}
                 <p className='ff-riffic clr-primary fs-25'>
-                    {getGuestName(guests)}
+                    {getGuestName(guests, guests.length > 1)}
                 </p>
                 {props.contestant.alias && (<p className='fs-16'>
                     ({props.contestant.alias})
