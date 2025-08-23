1e6805fffab7e876e840794a274c0c240275b498
Precious Olusola <preciousolusola16@gmail.com>
2025-08-23 22:50:17 +0100

    Sample Demo up

--- src/app/(FixedWidthLayout)/check-in/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TicketCheckInList from '@/components/Purchase/TicketCheckIn';
-
-export default function CheckInPage() {
-    
-    return <TicketCheckInList />;
-}
--- src/app/(FixedWidthLayout)/protocol/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TicketProtocolList from '@/components/Purchase/TicketProtocol';
-
-export default function CheckInPage() {
-    
-    return <TicketProtocolList />;
-}
